fa1f53429f1e17604ef123aca6685d998a55568a
Roman <romanranniew@gmail.com>
2023-09-15 10:15:44 +0200

    Init project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HPLanding from '@/views/HPLanding.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'landing',
+      component: HPLanding
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/picker',
+      name: 'picker',
+      component: () => import('../views/HPPicker.vue')
+    },
+    {
+      path: '/form',
+      name: 'form',
+      component: () => import('../views/HPForm.vue')
     }
   ]
 })
